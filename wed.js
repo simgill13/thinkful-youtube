fc8917654c4cdc152390fa96464ed36ddcd876ef
Sim Gill <simgill89@gmail.com>
2017-02-08 09:51:55 -0800

    tried and failed

--- wed.js
+++ wed.js
@@ -3,7 +3,7 @@ var OMDB_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
 function getDataFromApi(searchTerm, callback) {
   console.log(searchTerm);
   var query = {
-    q: 'searchTerm',
+    q: searchTerm,
     key: 'AIzaSyASKTdVhfhbiBkbUPi92NOLGmglcIlbu3o',
     part: 'snippet'
   }
@@ -13,15 +13,21 @@ function getDataFromApi(searchTerm, callback) {
 
 
 
-
 function displayOMDBSearchData(data) {
-
-  for (var i = 0; i < data.items.length; i++) {
-   console.log(data.items[i].snippet.thumbnails.medium.url);
-  }
-   
-
+var list= []
+    for (var i = 0; i < data.items.length; i++) {
+        
+        var thumbnails = data.items[i].snippet.thumbnails.medium.url
+        list.push(thumbnails);
+        
+    }
+    $('.js-search-results').html(`<ul> <li> ${list} </li> </ul>`);
+      
 }
+
+
+
+
 
 
 
@@ -36,4 +42,34 @@ function watchSubmit() {
 }
 
 $(function(){watchSubmit();});
+
+
+
+
+// function renderItem(item, itemId, itemTemplate, itemDataAttr) {
+//     var element = $(itemTemplate);
+//     element.find('.js-shopping-item').text(item.displayName);
+//     if (item.checkedOff) {
+//         element.find('.js-shopping-item').addClass('shopping-item__checked');
+//     }
+//     element.find('.js-shopping-item-toggle')
+//     element.attr(itemDataAttr, itemId);
+//     return element;
+// }
+
+// function renderList(state, listElement, itemDataAttr) {
+//     var itemsHTML = state.items.map(
+//         function(item, index) {
+//             return renderItem(item, index, listItemTemplate, itemDataAttr);
+//         });
+//     listElement.html(itemsHTML);
+// }
+
+
+
+
+
+// $('.js-search-results').find('img').attr('src', thumbnails);
+
+
 
